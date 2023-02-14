d33bde30951e8dfe338beb71c8aef8e83d1ea684
renhou <33872354+kensoz@users.noreply.github.com>
2023-02-14 16:11:03 +0900

    Add files via upload

---
+++ src/Index.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+  const lai: number = 1
+  return (
+    <div>
+      <h1 className='p-3 text-3xl font-bold underline'>{lai}</h1>
+    </div>
+  )
+}
+
+export default App
